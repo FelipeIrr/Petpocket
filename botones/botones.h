53c0137af1d3787d585cff691e0f3734bb94f3e7
benjaminIribarren <you@example.com>
2025-06-27 20:03:34 -0400

    comentarios agregados

--- botones/botones.h
+++ botones/botones.h
@@ -14,18 +14,18 @@ typedef enum { //MODULARIZAR ESTRUCTURAS
 
 typedef struct Item {
     char* nombre;        
-    TipoItem tipo; 
+    TipoItem tipo;  // tipo de item
     int precio;
-    int valor_energetico;
+    int valor_energetico; // si es tipo comida
     Texture2D aspecto;  // si es tipo aspecto  
     char* ruta_imagen; // Ruta de la imagen del ítem
 } Item;
 
 typedef struct Escenario {
     char* nombreEscenario;
-    Texture2D imagen_fondo;
-    int req_energia;
-    int req_monedas;
+    Texture2D imagen_fondo; // Imagen de fondo del escenario
+    int req_energia; // Requisitos de energía para acceder al escenario
+    int req_monedas; // Requisitos de monedas para acceder al escenario
     Map* tienda; //Mapa para la tienda
 } Escenario;
 
@@ -34,27 +34,27 @@ typedef struct Mascota {
     int energia;
     int monedas;
     List* inventario; // ítems en inventario
-    Texture2D aspecto_actual;
+    Texture2D aspecto_actual; // Aspecto actual de la mascota
     Escenario* escenarioActual; // Escenario actual de la mascota
 } Mascota;
 
 
-Mascota* crearMascota();
+Mascota* crearMascota(); // crea una mascota y le asigna un nombre
 
-void reiniciar(Mascota**);
+void reiniciar(Mascota**); // reinicia la mascota y su inventario
 
-Array* cargarEscenarios();
+Array* cargarEscenarios(); // carga los escenarios desde un archivo CSV y devuelve un Array de Escenarios
 
-void cargarItemsTienda(Map* tienda);
+void cargarItemsTienda(Map* tienda); // carga los ítems de la tienda desde un archivo CSV y los inserta en el mapa de la tienda
 
-void crearTienda(Escenario* escenario);
+void crearTienda(Escenario* escenario); // crea la tienda para un escenario dado
 
-void mostrarTienda(Mascota* mascota, Escenario* escenario);
+void mostrarTienda(Mascota* mascota, Escenario* escenario); // muestra la tienda del escenario actual de la mascota
 
-void cambiarEscenario(Mascota* mascota, Array* escenarios);
+void cambiarEscenario(Mascota* mascota, Array* escenarios); // cambia el escenario actual de la mascota a otro escenario disponible en el Array de escenarios
 
-void cambiarAspectoMascota(Mascota* mascota);
+void cambiarAspectoMascota(Mascota* mascota); // cambia el aspecto de la mascota a uno seleccionado por el usuario
 
-void alimentarMascota(Mascota* mascota);
+void alimentarMascota(Mascota* mascota); // alimenta a la mascota con un ítem de comida del inventario
 
 #endif
